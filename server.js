0377b1876e178b2f72ee2347f4928d2024da7d58
22-62286-cmyk <22-62286@g.batstate-u.edu.ph>
2025-11-21 09:46:40 +0000

    Update Message

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.write("JASMIN L. LURILLA ");
   res.write("BSBA 4102.");
+   res.write("FREELANCE MAKEUP ARTIST");
+   res.write("TIME IS GOLD. THATS WHY RESPECT MY TIME");
   res.end();
 });
 
